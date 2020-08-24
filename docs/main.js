f249a7c45e04ecb23fe0f762296583054045995a
ZippyMagician <zippymagician1@gmail.com>
2020-08-24 17:00:52 -0400

    Add a feature to support multiple test cases

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,25 @@
+// Newlines separate different test suites
 function runArn(t, inp) {
-    try {
-        return sprintf(parse(t, {stdin: inp || ""}));
-    } catch (error) {
-        return t + "\n" + error;
+    inp = inp.split("\n");
+    if (inp.length === 1) {
+        try {
+            return sprintf(parse(t, {stdin: inp[0] || ""}));
+        } catch (error) {
+            return t + "\n" + error;
+        }
+    } else {
+        let output = "";
+        let count = 1;
+        for (let suite of inp) {
+            output += `* Case ${count++}:\n`;
+            try {
+                output += sprintf(parse(t, {stdin: suite || ""}));
+            } catch (error) {
+                output += t + "\n" + error;
+            }
+            output += "\n";
+        }
+
+        return output;
     }
 }
