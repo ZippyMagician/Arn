89f181391b37e695e9eb2f65c6e34fe58fa01af4
ZippyMagician <zippymagician1@gmail.com>
2020-11-01 09:03:58 -0500

    :i never had a precedence level

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { pack, unpack, isPacked } = require('./compressor.js');
 const { decompress } = require('./dictionary.js');
 const { tokenize } = require('./lexer.js');
-const { makeAST: to_ast } = require('./ast.js');
+const { makeAST: toAST } = require('./ast.js');
 const { walkTree: parse } = require('./parse.js');
 
 const { printf } = require('./formatter.js');
@@ -9,6 +9,8 @@ const { printf } = require('./formatter.js');
 module.exports.run = (code, opts) => {
     if (opts.d) {
         console.log("Program:", code);
+        console.log("Tokens:", tokenize(code));
+        console.log("AST:", JSON.stringify(toAST(tokenize(code), code)));
     }
     if (opts.c) {
         console.log("Packed:", pack(code));
@@ -17,11 +19,11 @@ module.exports.run = (code, opts) => {
 
     if (isPacked(code.replace(/\n/g, " ").replace(/\s{2,}/g, " "))) code = unpack(code);
     if (opts.m) code = `{${code}}\\`;
-    printf(parse(to_ast(tokenize(code), code), opts, code));
+    printf(parse(toAST(tokenize(code), code), opts, code));
 }
 
 module.exports.parse = (code, opts) => {
     if (isPacked(code)) code = unpack(code);
     if (opts.m) code = `{${code}}\\`;
-    return parse(to_ast(tokenize(code), code), opts, code);
+    return parse(toAST(tokenize(code), code), opts, code);
 }
