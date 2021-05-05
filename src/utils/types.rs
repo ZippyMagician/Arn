4b92456ee09b18af0a8785eb71ef28af3ef56f3d
ZippyMagician <zippymagician1@gmail.com>
2021-05-05 16:34:43 -0400

    Print numbers in Arn formatting

--- src/utils/types.rs
+++ src/utils/types.rs
@@ -352,9 +352,9 @@ impl Display for Dynamic {
                     "{}",
                     if s.contains('.') && !s.contains('e') {
                         // First remove trailing zeros, then the dot if that was everything
-                        s.trim_end_matches('0').trim_end_matches('.')
+                        &s.trim_end_matches('0').trim_end_matches('.').replace('-', "_")
                     } else {
-                        &s
+                        &s.replace('-', "_")
                     }
                 )
             }
