18c3e1f94338beff0f7b2bfbdcb32df566fb9d48
ZippyMagician <zippymagician1@gmail.com>
2021-05-22 18:13:46 -0400

    Clean up examples

--- tools/yank_fixes.js
+++ tools/yank_fixes.js
@@ -6,10 +6,12 @@ async function yank() {
     let help = fs.readFileSync(__dirname + "/../src/parser.rs").toString();
     let defs = {};
 
-    consts.replace(/['"](.{1,2})['"]: (.{1,2}); (.-.)/g, (_, name, prec, ranks) => defs[name.replace("\\\\", "\\")] = [prec, ranks.split("-")]);
-    help.replace(/\/\/\s*(.+)\n\s*\"(.{1,2})\" =>/g, (_, help, name) => defs[name.replace("\\\\", "\\")].push(help));
-    help.replace(/\/\/\s*(.+)\n\s*\"(.{1,2})\" \| \"(.{1,2})\" =>/g, (_, helps, n1, n2) => {
+    consts.replace(/['"](.{1,3})['"]: (.{1,2}); (.-.)/g, (_, name, prec, ranks) => defs[name.replace("\\\\", "\\")] = [prec, ranks.split("-")]);
+    help.replace(/\/\/\s*(.+)\n\s*\"(.{1,3})\" =>/g, (_, help, name) => defs[name.replace("\\\\", "\\")].push(help));
+    help.replace(/\/\/\s*(.+)\n\s*\"(.{1,3})\" \| \"(.{1,3})\" =>/g, (_, helps, n1, n2) => {
         helps = helps.split(",").map(n => n.trim());
+        n1 = n1.replace("\\\\", "\\");
+        n2 = n2.replace("\\\\", "\\");
         defs[n1].push(helps[0]);
         defs[n2].push(helps[1]);
     });
